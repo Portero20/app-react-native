28aa2712df12e40df54771dee4f98749890d1d9e
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-01-22 21:17:40 -0300

    terminando desafio

--- src/components/task/item/index.jsx
+++ src/components/task/item/index.jsx
@@ -1,4 +1,4 @@
-import {Button, Text, TouchableOpacity} from 'react-native';
+import {Button, Text, TouchableOpacity, TouchableWithoutFeedback} from 'react-native';
 
 import React from "react";
 import {styles} from './styles';
@@ -6,10 +6,20 @@ import {styles} from './styles';
 const TaskItem = ({item, onHandlerModal}) => {
 
     return(
-        <TouchableOpacity style={styles.itemContainer} onPress={() => onHandlerModal(item)}>
-            <Button accessibilityRole='checkbox'/>
-            <Text style={styles.itemList}>{item.value}</Text>
-        </TouchableOpacity>
+
+        <View>
+
+            <TouchableWithoutFeedback onPress={() => alert('Pressed')} accessibilityRole='checkbox'>
+                <Text>Check</Text>
+            </TouchableWithoutFeedback>
+
+            <TouchableOpacity style={styles.itemContainer} onPress={() => onHandlerModal(item)}>
+                <Text style={styles.itemList}>{item.value}</Text>
+            </TouchableOpacity>
+
+        </View>
+
+        
     )
 
 }
